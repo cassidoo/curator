2c19d2ee62d8ab6780574e99fb64df71444f581f
cassidoo <cassidy@clarifai.com>
2016-03-20 21:37:21 -0400

    Add modal to project

--- js/components/opening.js
+++ js/components/opening.js
@@ -2,22 +2,42 @@
 
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Modal = require('react-modal');
 
 class Opening extends React.Component {
+  constructor() {
+    super();
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.state = { modalIsOpen: false };
+  }
+
+  openModal() {
+    this.setState({ modalIsOpen: true });
+  }
+
+  closeModal() {
+    this.setState({ modalIsOpen: false });
+  }
+
   render() {
     return (
       <div className="opening">
       <h1 className="opening">Curator</h1>
       <div className="opening-buttons">
-        <button onClick={this.showAboutModal}>How to Play</button>
+        <button onClick={this.openModal}>How to Play</button>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onRequestClose={this.closeModal}>
+          <h2>Hello</h2>
+          <button onClick={this.closeModal}>close</button>
+          <div>I am a modal</div>
+        </Modal>
         <button>New Game</button>
         <button>Load Game</button>
       </div>
     </div>
     );
-  }
-
-  showAboutModal() {
   }
 }
 
